0a59c52aeb13fc068e0c3e7b92b9a99ca1d5b41c
Konrad <wesolowskikonrad28@gmail.com>
2024-01-06 15:27:08 +0100

    test

--- WebApplication/views.py
+++ WebApplication/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_view(request):
-    return render(request, 'main.html')
+    return render(request, "main.html")
+
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, "login.html")
