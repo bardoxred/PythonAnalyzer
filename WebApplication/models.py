ca261c42aeed7407d22261372ea27d320acd84cf
Konrad <wesolowskikonrad28@gmail.com>
2024-01-16 20:05:43 +0100

    Refactoring model and views.py for sign in

--- WebApplication/models.py
+++ WebApplication/models.py
@@ -1,11 +1,39 @@
+import uuid
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
 
-class User(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
+
+
+class Users(AbstractBaseUser):
     user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    last_refresh = models.DateTimeField(null=True, blank=True)
 
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return self.username
--- WebApplication/views.py
+++ WebApplication/views.py
@@ -1,6 +1,10 @@
+import uuid
 from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from WebApplication.models import User
+from django.utils import timezone
+
+from WebApplication.models import Users
 from django.contrib.auth import authenticate, login, logout
 
 
@@ -13,10 +17,13 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.get(username=username, password=password)
-
+        try:
+            user = Users.objects.get(username=username, password=password)
+        except:
+            user = None
+        print(user.__str__)
         if user is not None:
-
+            login(request, user)
             return render(request, "main.html")
         else:
             return redirect('register')
@@ -35,10 +42,20 @@ def register_user(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = User.objects.create(first_name=first_name, last_name=last_name, username=username, password=password)
+        user = Users.objects.create(first_name=first_name, last_name=last_name, username=username, password=password)
         user.save()
         messages.success(request, "Your account has been successfully created")
 
         return redirect('login')
 
     return render(request, "register.html")
+
+def check_token(request):
+    if request.user.is_authentiacted and request.user.token_expiration > timezone.now():
+        request.user.token_expiration = timezone.now() + timezone.timedelta(minutes=10)
+        request.user.save()
+        return HttpResponse("Użytkownik się zalogował")
+    else:
+        return HttpResponse("Użytkownik niezalogowany albo token wygasł")
+
+
